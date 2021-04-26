945443e719a30d881f39578a99742be94f25f4e7
Mitchell Edmunds <Mitchell.Edmunds@ext.ons.gov.uk>
2021-04-26 13:59:20 +0100

    Standardise row comparison by making precision optional
    The objective of the issue was to make allow_nan_equality available to
    assert_approx_df_equality, but on reviewing the code it seemed like a
    more efficient plan to include the precision option in
    assert_df_equality and make it optional. This results in only one set
    of functions needed to be maintained.
    
    I've moved the single assert_rows_equality function into row_comparer
    along with the custom exception which I've renamed. I've made one
    generic equality function which takes options. All the tests have been
    updated although there could be scope for a few more new tests - I
    wanted to consult on that first.
    
    This is a fairly large change to the API for which a major version
    update would be recommended.

--- tests/test_dataframe_comparer.py
+++ tests/test_dataframe_comparer.py
@@ -2,16 +2,18 @@ import pytest
 
 from spark import *
 from chispa import *
-from chispa.dataframe_comparer import are_dfs_equal
 from chispa.schema_comparer import SchemasNotEqualError
+from chispa.row_comparer import RowsNotEqualError
 
 
 def describe_assert_df_equality():
     def it_throws_with_schema_mismatches():
         data1 = [(1, "jose"), (2, "li"), (3, "laura")]
         df1 = spark.createDataFrame(data1, ["num", "expected_name"])
+
         data2 = [("bob", "jose"), ("li", "li"), ("luisa", "laura")]
         df2 = spark.createDataFrame(data2, ["name", "expected_name"])
+
         with pytest.raises(SchemasNotEqualError) as e_info:
             assert_df_equality(df1, df2)
 
@@ -19,8 +21,10 @@ def describe_assert_df_equality():
     def it_can_work_with_different_row_orders():
         data1 = [(1, "jose"), (2, "li")]
         df1 = spark.createDataFrame(data1, ["num", "name"])
+
         data2 = [(2, "li"), (1, "jose")]
         df2 = spark.createDataFrame(data2, ["num", "name"])
+
         assert_df_equality(df1, df2, transforms=[lambda df: df.sort(df.columns)])
 
 
@@ -45,7 +49,7 @@ def describe_assert_df_equality():
         df1 = spark.createDataFrame(data1, ["num", "name"])
         data2 = [(2, "li"), (1, "jose")]
         df2 = spark.createDataFrame(data2, ["num", "name"])
-        with pytest.raises(DataFramesNotEqualError):
+        with pytest.raises(RowsNotEqualError):
             assert_df_equality(df1, df2, transforms=[lambda df: df.sort(df.columns)])
 
 
@@ -82,7 +86,7 @@ def describe_assert_df_equality():
         df1 = spark.createDataFrame(data1, ["name", "expected_name"])
         data2 = [("bob", "jose"), ("li", "li"), ("luisa", "laura")]
         df2 = spark.createDataFrame(data2, ["name", "expected_name"])
-        with pytest.raises(DataFramesNotEqualError) as e_info:
+        with pytest.raises(RowsNotEqualError) as e_info:
             assert_df_equality(df1, df2)
 
 
@@ -91,7 +95,7 @@ def describe_assert_df_equality():
         df1 = spark.createDataFrame(data1, ["name", "expected_name"])
         data2 = [("jose", "jose"), ("li", "li")]
         df2 = spark.createDataFrame(data2, ["name", "expected_name"])
-        with pytest.raises(DataFramesNotEqualError) as e_info:
+        with pytest.raises(RowsNotEqualError) as e_info:
             assert_df_equality(df1, df2)
 
 
@@ -108,33 +112,8 @@ def describe_assert_df_equality():
         df1 = spark.createDataFrame(data1, ["num", "name"])
         data2 = [(float('nan'), "jose"), (2.0, "li")]
         df2 = spark.createDataFrame(data2, ["num", "name"])
-        with pytest.raises(DataFramesNotEqualError) as e_info:
+        with pytest.raises(RowsNotEqualError) as e_info:
             assert_df_equality(df1, df2, allow_nan_equality=False)
-
-
-def describe_are_dfs_equal():
-    def it_returns_false_with_schema_mismatches():
-        data1 = [(1, "jose"), (2, "li"), (3, "laura")]
-        df1 = spark.createDataFrame(data1, ["num", "expected_name"])
-        data2 = [("bob", "jose"), ("li", "li"), ("luisa", "laura")]
-        df2 = spark.createDataFrame(data2, ["name", "expected_name"])
-        assert are_dfs_equal(df1, df2) == False
-
-
-    def it_returns_false_with_content_mismatches():
-        data1 = [("jose", "jose"), ("li", "li"), ("luisa", "laura")]
-        df1 = spark.createDataFrame(data1, ["name", "expected_name"])
-        data2 = [("bob", "jose"), ("li", "li"), ("luisa", "laura")]
-        df2 = spark.createDataFrame(data2, ["name", "expected_name"])
-        assert are_dfs_equal(df1, df2) == False
-
-
-    def it_returns_true_when_dfs_are_same():
-        data1 = [("bob", "jose"), ("li", "li"), ("luisa", "laura")]
-        df1 = spark.createDataFrame(data1, ["name", "expected_name"])
-        data2 = [("bob", "jose"), ("li", "li"), ("luisa", "laura")]
-        df2 = spark.createDataFrame(data2, ["name", "expected_name"])
-        assert are_dfs_equal(df1, df2) == True
 
 
 def describe_assert_approx_df_equality():
@@ -143,8 +122,8 @@ def describe_assert_approx_df_equality():
         df1 = spark.createDataFrame(data1, ["num", "expected_name"])
         data2 = [(1.0, "jose"), (1.05, "li"), (1.0, "laura"), (None, "hi")]
         df2 = spark.createDataFrame(data2, ["num", "expected_name"])
-        with pytest.raises(DataFramesNotEqualError) as e_info:
-            assert_approx_df_equality(df1, df2, 0.1)
+        with pytest.raises(RowsNotEqualError) as e_info:
+            assert_df_equality(df1, df2, precision=0.1)
 
 
     def it_throws_with_with_length_mismatch():
@@ -152,8 +131,8 @@ def describe_assert_approx_df_equality():
         df1 = spark.createDataFrame(data1, ["num", "expected_name"])
         data2 = [(1.0, "jose"), (1.05, "li")]
         df2 = spark.createDataFrame(data2, ["num", "expected_name"])
-        with pytest.raises(DataFramesNotEqualError) as e_info:
-            assert_approx_df_equality(df1, df2, 0.1)
+        with pytest.raises(RowsNotEqualError) as e_info:
+            assert_df_equality(df1, df2, precision=0.1)
 
 
     def it_does_not_throw_with_no_mismatch():
@@ -161,5 +140,4 @@ def describe_assert_approx_df_equality():
         df1 = spark.createDataFrame(data1, ["num", "expected_name"])
         data2 = [(1.0, "jose"), (1.05, "li"), (1.2, "laura"), (None, None)]
         df2 = spark.createDataFrame(data2, ["num", "expected_name"])
-        assert_approx_df_equality(df1, df2, 0.1)
-
+        assert_df_equality(df1, df2, precision=0.1)
--- tests/test_row_comparer.py
+++ tests/test_row_comparer.py
@@ -1,28 +1,32 @@
-import pytest
-
-from spark import *
-from chispa.row_comparer import *
+from chispa.row_comparer import are_rows_equal
 from pyspark.sql import Row
 
 
-def test_are_rows_equal():
-    assert are_rows_equal(Row("bob", "jose"), Row("li", "li")) == False
-    assert are_rows_equal(Row("luisa", "laura"), Row("luisa", "laura")) == True
-    assert are_rows_equal(Row(None, None), Row(None, None)) == True
-
-def test_are_rows_equal_enhanced():
-    assert are_rows_equal_enhanced(Row(n1 = "bob", n2 = "jose"), Row(n1 = "li", n2 = "li"), False) == False
-    assert are_rows_equal_enhanced(Row(n1 = "luisa", n2 = "laura"), Row(n1 = "luisa", n2 = "laura"), False) == True
-    assert are_rows_equal_enhanced(Row(n1 = None, n2 = None), Row(n1 = None, n2 = None), False) == True
-
-    assert are_rows_equal_enhanced(Row(n1="bob", n2="jose"), Row(n1="li", n2="li"), True) == False
-    assert are_rows_equal_enhanced(Row(n1=float('nan'), n2="jose"), Row(n1=float('nan'), n2="jose"), True) == True
-    assert are_rows_equal_enhanced(Row(n1=float('nan'), n2="jose"), Row(n1="hi", n2="jose"), True) == False
+def describe_are_rows_equal():
+    def returns_False_when_string_values_are_not_equal():
+        assert are_rows_equal(Row(n1="bob", n2="jose"), Row(n1="li", n2="li")) == False
+    def returns_True_when_string_values_are_equal():
+        assert are_rows_equal(Row(n1="luisa", n2="laura"), Row(n1="luisa", n2="laura")) == True
+    def returns_True_when_both_rows_are_None():
+        assert are_rows_equal(Row(n1=None, n2=None), Row(n1=None, n2=None)) == True
 
 
-def test_are_rows_approx_equal():
-    assert are_rows_approx_equal(Row(num = 1.1, first_name = "li"), Row(num = 1.05, first_name = "li"), 0.1) == True
-    assert are_rows_approx_equal(Row(num = 5.0, first_name = "laura"), Row(num = 5.0, first_name = "laura"), 0.1) == True
-    assert are_rows_approx_equal(Row(num = 5.0, first_name = "laura"), Row(num = 5.9, first_name = "laura"), 0.1) == False
-    assert are_rows_approx_equal(Row(num = None, first_name = None), Row(num = None, first_name = None), 0.1) == True
+def describe_are_rows_equal_when_allowing_nan_equality():
+    def returns_False_when_no_NaN_values_to_compare_and_other_values_are_not_equal():
+        assert are_rows_equal(Row(n1="bob", n2="jose"), Row(n1="li", n2="li"), allow_nan_equality=True) == False
+    def returns_True_when_either_the_values_are_equal_or_both_nan():
+        assert are_rows_equal(Row(n1=float('nan'), n2="jose"), Row(n1=float('nan'), n2="jose"), allow_nan_equality=True) == True
+    def returns_False_when_comparing_nan_to_string():
+        assert are_rows_equal(Row(n1=float('nan'), n2="jose"), Row(n1="hi", n2="jose"), allow_nan_equality=True) == False
+
+
+def describe_are_rows_equal_when_given_precision():
+    def returns_True_when_float_value_difference_is_less_than_precision():
+        assert are_rows_equal(Row(num = 1.1, first_name = "li"), Row(num = 1.05, first_name = "li"), precision=0.1) == True
+    def returns_True_when_float_values_are_exactly_equal():
+        assert are_rows_equal(Row(num = 5.0, first_name = "laura"), Row(num = 5.0, first_name = "laura"), precision=0.1) == True
+    def returns_False_when_float_value_difference_is_more_than_precision():
+        assert are_rows_equal(Row(num = 5.0, first_name = "laura"), Row(num = 5.9, first_name = "laura"), precision=0.1) == False
+    def returns_True_when_all_values_are_Nones():
+        assert are_rows_equal(Row(num = None, first_name = None), Row(num = None, first_name = None), precision=0.1) == True
 
