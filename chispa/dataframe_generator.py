6a013e59847e6b71a2d6d75eccf11e946635ea01
Gonçalo Castro <goncalo.andre.carneiro.de.castro@nl.abnamro.com>
2022-01-24 17:16:04 +0100

    Add possibility to use kwargs for providers.

--- chispa/dataframe_generator.py
+++ chispa/dataframe_generator.py
@@ -1,17 +1,53 @@
 import copy
-import uuid
 from dataclasses import dataclass, field
-from functools import reduce
-from typing import List, Dict, Iterator, Any
+from typing import List, Iterator, Any, Optional, Hashable
 
-import cloudpickle
-import pyspark.serializers
 from faker import Factory, Generator
 from pyspark.sql import DataFrame, SparkSession
-from pyspark.sql.functions import udf
 from pyspark.sql.types import StructType, StructField
 
-pyspark.serializers.cloudpickle = cloudpickle
+# TODO add missing Spark types.
+DEFAULT_CONFIG: dict = {
+    "StringType": {"provider": "pystr"},
+    "ByteType": {"provider": "pyint", "kwargs": {"min_value": -128, "max_value": 127}},
+    "ShortType": {"provider": "pyint", "kwargs": {"min_value": -32768, "max_value": 32767}},
+    "IntegerType": {"provider": "pyint", "kwargs": {"min_value": -2147483648, "max_value": 2147483647}},
+    "LongType": {"provider": "pyint", "kwargs": {"min_value": -9223372036854775808, "max_value": 9223372036854775807}},
+    "DoubleType": {"provider": "pyfloat"},
+    "FloatType": {"provider": "pyfloat"},
+    "DecimalType(10,0)": {"provider": "pydecimal", "kwargs": {"left_digits": 10, "right_digits": 0}},
+    "DateType": {"provider": "date_object"},
+    "TimestampType": {"provider": "date_time"},
+    "BooleanType": {"provider": "pybool"},
+    "BinaryType": {"provider": "binary", "kwargs": {"length": 64}}
+}
+
+
+class DataTypeMissingException(Exception):
+    """DataType missing from default config"""
+
+    def __init__(self):
+        super().__init__("DataType missing from default config")
+
+
+class PropertyCheckException(Exception):
+    """Property Check failed"""
+
+    def __init__(self, message):
+        super().__init__(f"Property Check failed: {message}")
+
+
+@dataclass
+class Report:
+    property_check: bool
+    dataframe: DataFrame
+
+
+@dataclass
+class DataConfig:
+    data_type: str
+    provider: str
+    kwargs: dict = field(default_factory=dict)
 
 
 def default_field(obj):
@@ -19,28 +55,6 @@ def default_field(obj):
     Method to deal with complex objects when using default values for dataclasses.
     """
     return field(default_factory=lambda: copy.copy(obj))
-
-
-def for_all(dfs: Iterator[DataFrame], property_to_check: Any) -> Iterator[bool]:
-    """
-    Method that will run a property over all the DataFrames in the iterator and returns an iterator
-    with the boolean result.
-
-    :param dfs: A generator with DataFrames.
-    :param property_to_check: A function that has a DataFrame as a parameter and returns a bool.
-    :return:
-    """
-    return map(lambda df: property_to_check(df), dfs)
-
-
-def check_property(reports: Iterator[bool]):
-    """
-    Method that will assert if the property that ran over the DataFrames is true for all of them.
-
-    :param reports:
-    :return:
-    """
-    assert all(list(reports)) is True
 
 
 @dataclass
@@ -60,61 +74,25 @@ class DataFrameGenerator:
         }}
     """
     schema: StructType
+    transformer: Any = None
     num_dataframes: int = 10
+    default_config: dict = default_field(DEFAULT_CONFIG)
     config: dict = field(default_factory=dict)
-    id_column_name = str(uuid.uuid4()).replace("-", "_")
-    num_records: int = 100
+    num_records: int = 10
     faker: Generator = Factory.create()
-    faker_spark_types: Dict[str, str] = default_field({
-        "StringType": "text",
-        "IntegerType": "random_int",
-        "LongType": "random_int",
-        "DateType": "date_object"
-    })
+    seed: Optional[Hashable] = None
 
-    # TODO the data in the DataFrame needs to be randomized more, perhaps check for Faker return types and use that
-    # to randomize the data in the columns a bit more.
-    def arbitrary_dataframe_spark(self) -> Iterator[DataFrame]:
+    def arbitrary_dataframes(self) -> Iterator[DataFrame]:
         """
         Method to generate a number of DataFrames (default is 10).
         :return: An iterator with DataFrames.
         """
-        return map(lambda x: self.generate_in_spark(), range(0, self.num_dataframes))
+        return map(
+            lambda x: self.transformer(self.generate_data()) if self.transformer is not None else self.generate_data(),
+            range(0, self.num_dataframes)
+        )
 
-    def generate_initial_dataframe(self) -> DataFrame:
-        """
-        Method to generate the initial DataFrame consisting of one column with a certain number of records.
-        """
-        data = [(elem,) for elem in range(0, self.num_records)]
-        spark: SparkSession = SparkSession.builder.getOrCreate()
-        return spark.createDataFrame(data, [self.id_column_name])
-
-    def generate_in_spark(self) -> DataFrame:
-        """
-        Method that will take the Spark DataFrame schema provided to the dataclass,
-        iterate over it's fields and call the generate_fake_data method.
-        """
-        return reduce(
-            lambda dataframe, schema_field:
-            self.generate_fake_data(dataframe=dataframe, schema_field=schema_field), self.schema,
-            self.generate_initial_dataframe()
-        ).drop(self.id_column_name)
-
-    def generate_fake_data(self, dataframe: DataFrame, schema_field: StructField) -> DataFrame:
-        """
-        Method that will generate fake data for a specific field and add it to the DataFrame provided using UDFs.
-        A config can be pass with a specific provider for a certain column.
-
-        :param dataframe: The DataFrame to add data to.
-        :param schema_field: The field from the schema to add to the DataFrame.
-        :return: The DataFrame with the column added.
-        """
-        data_type, provider = self.get_datatype_and_provider(schema_field)
-        function = getattr(self.faker, provider)
-        fake_factory_udf = udf(function, data_type)
-        return dataframe.withColumn(schema_field.name, fake_factory_udf())
-
-    def get_datatype_and_provider(self, schema_field: StructField):
+    def get_datatype_and_provider(self, schema_field: StructField) -> DataConfig:
         """
         Helper function to get the needed datatype and Faker provider for a specific field.
 
@@ -122,24 +100,74 @@ class DataFrameGenerator:
         :return: The data type of the field and Faker provider.
         """
         if schema_field.name in self.config:
-            data_type = self.config[schema_field.name].get("data_type")
+            data_type = str(self.config[schema_field.name].get("data_type"))
             provider = self.config[schema_field.name].get("provider")
+            kwargs = self.config[schema_field.name].get("kwargs")
         else:
-            data_type = schema_field.dataType
-            provider = self.faker_spark_types.get(str(schema_field.dataType))
-        return data_type, provider
+            data_type = str(schema_field.dataType)
+            config: dict = self.default_config.get(data_type)
+            if config is not None:
+                provider = config.get("provider")
+            else:
+                raise DataTypeMissingException
+            kwargs: dict = config.get("kwargs", {})
+        return DataConfig(data_type=data_type, provider=provider, kwargs=kwargs)
 
-    def generate_locally(self) -> DataFrame:
+    def use_providers(self, data_config: DataConfig):
+        """
+        Method to make use of the Faker providers to generate the data.
+
+        :param data_config: DataConfig object.
+        :return: Values from the Faker providers.
+        """
+        if self.seed is not None:
+            self.faker.seed(seed=self.seed)
+        return getattr(self.faker, data_config.provider)(**data_config.kwargs) if data_config.kwargs else getattr(
+            self.faker, data_config.provider)()
+
+    def generate_data(self) -> DataFrame:
         """
         Method that will generate fake data for for all fields and then put it into a DataFrame.
         A config can be pass with a specific provider for a certain column. This method does not use UDFs.
+
+        :return: A DataFrame from the data created by Faker.
         """
-        providers: List[str] = [self.get_datatype_and_provider(schema_field)[1] for schema_field in self.schema.fields]
-        data = map(
-            lambda x: list(map(
-                lambda provider:
-                getattr(self.faker, provider)(),
-                providers)),
-            range(1, self.num_records + 1))
+        data_configs: List[DataConfig] = [
+            self.get_datatype_and_provider(schema_field=schema_field) for schema_field in
+            self.schema.fields
+        ]
+        data: Iterator = map(lambda x: list(map(self.use_providers, data_configs)), range(0, self.num_records))
         spark: SparkSession = SparkSession.builder.getOrCreate()
-        return spark.createDataFrame(data, self.schema.names)
+        return spark.createDataFrame(data=data, schema=self.schema)
+
+
+def for_all(dfs: Iterator[DataFrame], property_to_check: Any) -> Iterator[Report]:
+    """
+    Method that will run a property over all the DataFrames in the iterator and returns an iterator
+    with the boolean result.
+
+    :param dfs: A generator with DataFrames.
+    :param property_to_check: A function that has a DataFrame as a parameter and returns a bool.
+    :return: An iterator of Report objects.
+    """
+    return map(lambda df: Report(property_check=property_to_check(df), dataframe=df), dfs)
+
+
+def check_property(reports: Iterator[Report], function_to_run_in_failure: Optional[Any] = None) -> bool:
+    """
+    Method that will assert if the property that ran over the DataFrames is true for all of them.
+
+    :param reports: The reports that contains the property result with the corresponding DataFrame.
+    :param function_to_run_in_failure: Optional function to run over the failed DataFrames.
+    :return: True if the property checked is true for all DataFrames, raises PropertyCheckException with the list
+    of failed DataFrames.
+    """
+    l: List[Report] = list(reports)
+    failed: List[DataFrame] = [elem.dataframe for elem in l if elem.property_check is False]
+    if failed:
+        if function_to_run_in_failure is not None:
+            for df in failed:
+                function_to_run_in_failure(df)
+        raise PropertyCheckException(f"\n{[elem.collect() for elem in failed]}")
+    else:
+        return True
--- tests/test_dataframe_generator.py
+++ tests/test_dataframe_generator.py
@@ -1,57 +1,203 @@
+from dataclasses import replace
+from typing import Iterator, List
+from unittest import TestCase
+
 from pyspark.sql import DataFrame
-from pyspark.sql.types import StringType, StructType, StructField, LongType, DateType
+from pyspark.sql.functions import lit
+from pyspark.sql.types import StringType, StructType, StructField, LongType, DateType, ByteType, ShortType, IntegerType, \
+    DecimalType, FloatType, DoubleType, BooleanType, BinaryType, TimestampType, ArrayType
 
-from chispa.dataframe_generator import DataFrameGenerator, for_all, check_property
-
-SCHEMA = StructType([
-    StructField("num", LongType(), True),
-    StructField("expected_name", StringType(), True),
-    StructField("date", DateType(), True)
-])
+from chispa.dataframe_generator import DataFrameGenerator, for_all, check_property, DataConfig, Report, \
+    PropertyCheckException, DataTypeMissingException
+from .spark import *
 
 
-def describe_dataframe_generator():
-    def test_generate_in_spark():
-        df_gen = DataFrameGenerator(schema=SCHEMA)
-        df_gen.generate_in_spark().show(truncate=False)
+class TestDataFrameGenerator(TestCase):
+    schema: StructType = StructType([
+        StructField("expected_name", StringType(), True),
+        StructField("int", IntegerType(), True),
+        StructField("long", LongType(), True),
+        StructField("byte", ByteType(), True),
+        StructField("short", ShortType(), True),
+        StructField("double", DoubleType(), True),
+        StructField("float", FloatType(), True),
+        StructField("decimal", DecimalType(), True),
+        StructField("bool", BooleanType(), True),
+        StructField("binary", BinaryType(), True),
+        StructField("date", DateType(), True),
+        StructField("timestamp", TimestampType(), True)
+    ])
+    df_gen: DataFrameGenerator = DataFrameGenerator(schema=schema, seed=0)
 
-    def test_generate_in_spark_one_million_records():
-        df_gen = DataFrameGenerator(schema=SCHEMA, num_records=1000000)
-        print(df_gen.generate_in_spark().count())
+    test_data = ([1, "dick smith"], [2, "jane smith"], [3, "nick smith"])
+    test_data_schema: StructType = StructType([
+        StructField('id', StringType(), True),
+        StructField('name', StringType(), True)
+    ])
+    test_df: DataFrame = spark.createDataFrame(data=test_data, schema=test_data_schema)
 
-    def test_generate_in_spark_with_config():
-        config = {"bank_account": {
-            "data_type": StringType(),
-            "provider": "iban"
-        }}
+    def test_for_all_given_no_dataframe_and_invalid_property_should_return_empty_list(self):
+        dfs: Iterator[DataFrame] = iter([])
+
+        actual: List[Report] = list(for_all(
+            dfs=dfs,
+            property_to_check=None)
+        )
+        expected: List[Report] = []
+
+        self.assertEquals(expected, actual)
+
+    def test_for_all_given_single_dataframe_and_valid_property_should_return_correct_report(self):
+        dfs: Iterator[DataFrame] = iter([self.test_df])
+
+        actual: List[Report] = list(for_all(
+            dfs=dfs,
+            property_to_check=lambda df: df.schema == self.test_data_schema and df.count() == 3)
+        )
+        expected: List[Report] = [Report(property_check=True, dataframe=self.test_df)]
+
+        self.assertEquals(expected, actual)
+
+    def test_for_all_given_multiple_dataframes_and_valid_property_should_return_correct_report(self):
+        dfs: Iterator[DataFrame] = iter([self.test_df, self.test_df])
+
+        actual: List[Report] = list(for_all(
+            dfs=dfs,
+            property_to_check=lambda df: df.schema == self.test_data_schema and df.count() == 3)
+        )
+        expected: List[Report] = [
+            Report(property_check=True, dataframe=self.test_df),
+            Report(property_check=True, dataframe=self.test_df)
+        ]
+
+        self.assertEquals(expected, actual)
+
+    def test_check_property_given_no_report_should_return_true(self):
+        reports: Iterator[Report] = iter([])
+
+        actual: bool = check_property(reports=reports)
+
+        self.assertTrue(actual)
+
+    def test_check_property_given_valid_report_should_return_true(self):
+        reports: Iterator[Report] = iter([Report(property_check=True, dataframe=self.test_df)])
+
+        actual: bool = check_property(reports=reports)
+
+        self.assertTrue(actual)
+
+    def test_check_property_given_invalid_report_should_raise_exception(self):
+        reports: Iterator[Report] = iter([Report(property_check=False, dataframe=self.test_df)])
+
+        self.assertRaises(PropertyCheckException, check_property, reports=reports)
+
+    def test_arbitrary_dataframes_should_return_correct_default_values_for_dataframes(self):
+        actual: List[DataFrame] = list(self.df_gen.arbitrary_dataframes())
+        length_check: bool = len(actual) == self.df_gen.num_dataframes
+        number_records_check: bool = all([elem.count() == self.df_gen.num_records for elem in actual])
+        schema_check: bool = all([elem.schema == self.schema for elem in actual])
+
+        self.assertTrue(all([length_check, number_records_check, schema_check]))
+
+    def test_arbitrary_dataframes_with_invalid_transformer_should_raise_exception(self):
+        df_gen_with_config: DataFrameGenerator = replace(
+            self.df_gen,
+            transformer=lambda x: x + 1
+        )
+
+        self.assertRaises(Exception, df_gen_with_config.arbitrary_dataframes())
+
+    def test_arbitrary_dataframes_with_transformer_should_return_correct_default_values_for_dataframes(self):
+        number_of_records: int = 10
+        new_column_name: str = "literal"
+        df_gen_with_config: DataFrameGenerator = replace(
+            self.df_gen,
+            transformer=lambda df: df.withColumn(new_column_name, lit(1)).limit(number_of_records)
+        )
         schema = StructType([
-            StructField("bank_account", StringType(), True)
+            StructField("expected_name", StringType(), True),
+            StructField("int", IntegerType(), True),
+            StructField("long", LongType(), True),
+            StructField("byte", ByteType(), True),
+            StructField("short", ShortType(), True),
+            StructField("double", DoubleType(), True),
+            StructField("float", FloatType(), True),
+            StructField("decimal", DecimalType(10, 0), True),
+            StructField("bool", BooleanType(), True),
+            StructField("binary", BinaryType(), True),
+            StructField("date", DateType(), True),
+            StructField("timestamp", TimestampType(), True),
+            StructField(new_column_name, IntegerType(), False)
         ])
-        df_gen = DataFrameGenerator(schema=schema, config=config)
-        df_gen.generate_in_spark().show(truncate=False)
 
-    def test_generate_locally():
-        df_gen = DataFrameGenerator(schema=SCHEMA)
-        df_gen.generate_locally().show(truncate=False)
+        actual: List[DataFrame] = list(df_gen_with_config.arbitrary_dataframes())
+        number_records_check: bool = all([elem.count() == number_of_records for elem in actual])
+        schema_check: bool = all([elem.schema == schema for elem in actual])
 
-    def test_generate_locally_one_million_records():
-        df_gen = DataFrameGenerator(schema=SCHEMA, num_records=1000000)
-        print(df_gen.generate_in_spark().count())
+        self.assertTrue(all([number_records_check, schema_check]))
 
-    def test_generate_locally_with_config():
-        config = {"bank_account": {
+    def test_get_datatype_and_provider_given_schema_field_with_no_expected_type_should_raise_exception(self):
+        schema_field: StructField = StructField("expected_name", ArrayType(StringType()), True)
+
+        self.assertRaises(DataTypeMissingException, self.df_gen.get_datatype_and_provider, schema_field=schema_field)
+
+    def test_get_datatype_and_provider_given_a_schema_field_should_return_correct_data_config_object(self):
+        schema_field: StructField = StructField("expected_name", StringType(), True)
+
+        actual: DataConfig = self.df_gen.get_datatype_and_provider(schema_field=schema_field)
+        expected: DataConfig = DataConfig(data_type="StringType", provider='pystr', kwargs={})
+
+        self.assertEquals(expected, actual)
+
+    def test_get_datatype_and_provider_given_a_schema_field_and_config_should_return_correct_data_config_object(self):
+        config = {"expected_name": {
             "data_type": StringType(),
-            "provider": "iban"
+            "provider": "random_element",
+            "kwargs": {
+                "elements": ('x', 'y')
+            }
         }}
-        schema = StructType([
-            StructField("bank_account", StringType(), True)
-        ])
-        df_gen = DataFrameGenerator(schema=schema, config=config)
-        df_gen.generate_locally().show(truncate=False)
+        df_gen_with_config: DataFrameGenerator = replace(self.df_gen, config=config)
+        schema_field: StructField = StructField("expected_name", StringType(), True)
 
-    def test_stuff():
-        df_gen = DataFrameGenerator(schema=SCHEMA, num_dataframes=2)
-        arbitrary_dfs = df_gen.arbitrary_dataframe_spark()
-        x = for_all(arbitrary_dfs, lambda df: df.schema == SCHEMA and df.count() >= 100)
-        check_property(x)
+        actual: DataConfig = df_gen_with_config.get_datatype_and_provider(schema_field=schema_field)
+        expected: DataConfig = DataConfig(
+            data_type="StringType",
+            provider='random_element',
+            kwargs={"elements": ('x', 'y')}
+        )
 
+        self.assertEquals(expected, actual)
+
+    def test_use_providers_given_data_config_object_should_return_the_correct_value(self):
+        data_config: DataConfig = DataConfig(data_type="StringType", provider="pystr")
+
+        actual: str = self.df_gen.use_providers(data_config=data_config)
+        expected: str = "RNvnAvOpyEVAoNGnVZQU"
+
+        self.assertEquals(expected, actual)
+
+    def test_use_providers_given_data_config_object_with_provider_and_keyword_arguments_should_return_the_correct_value(
+            self):
+        data_config: DataConfig = DataConfig(
+            data_type="StringType",
+            provider="random_element",
+            kwargs={
+                "elements": ('x', 'y')
+            }
+        )
+
+        actual: str = self.df_gen.use_providers(data_config=data_config)
+        expected: str = "y"
+
+        self.assertEquals(expected, actual)
+
+    def test_generate_data_given_a_known_schema_should_return_correct_dataframe(self):
+        actual: DataFrame = self.df_gen.generate_data()
+        actual_count: int = actual.count()
+        actual_schema: StructType = actual.schema
+
+        expected_count: int = 10
+        expected_schema: StructType = self.schema
+
+        self.assertTrue(actual_count == expected_count and actual_schema == expected_schema)
