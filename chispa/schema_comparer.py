e7734e0899992539992c301ff79138caed80ae88
Chris Williams <107429228+chris-remedy@users.noreply.github.com>
2022-08-18 11:00:05 +0100

    Ignore nullability for nested array types

--- chispa/schema_comparer.py
+++ chispa/schema_comparer.py
@@ -1,7 +1,6 @@
 from chispa.prettytable import PrettyTable
 from chispa.bcolors import *
 import chispa.six as six
-from chispa.structfield_comparer import are_structfields_equal
 
 
 class SchemasNotEqualError(Exception):
@@ -33,7 +32,7 @@ def assert_schema_equality_ignore_nullable(s1, s2):
         t = PrettyTable(["schema1", "schema2"])
         zipped = list(six.moves.zip_longest(s1, s2))
         for sf1, sf2 in zipped:
-            if are_structfields_equal(sf1, sf2, True):
+            if are_structfields_equal_ignore_nullable(sf1, sf2):
                 t.add_row([blue(sf1), blue(sf2)])
             else:
                 t.add_row([sf1, sf2])
@@ -45,25 +44,32 @@ def are_schemas_equal_ignore_nullable(s1, s2):
         return False
     zipped = list(six.moves.zip_longest(s1, s2))
     for sf1, sf2 in zipped:
-        names_equal = sf1.name == sf2.name
-        types_equal = check_type_equal_ignore_nullable(sf1, sf2)
-        if not names_equal or not types_equal:
-          return False
+        if not are_structfields_equal_ignore_nullable(sf1, sf2):
+            return False
     return True
 
 
-def check_type_equal_ignore_nullable(sf1, sf2):
-    """Checks StructField data types ignoring nullables.
+def are_structfields_equal_ignore_nullable(sf1, sf2):
+    if sf1 is None or sf2 is None:
+        if sf1 is None and sf2 is None:
+            return True
+        else:
+            return False
+    if sf1.name != sf2.name:
+        return False
+    else:
+        return are_datatypes_equal_ignore_nullable(sf1.dataType, sf2.dataType)
 
-    Handles array and struct element types also.
+def are_datatypes_equal_ignore_nullable(dt1, dt2):
+    """Checks if datatypes are equal, descending into structs and arrays to
+    ignore nullability.
     """
-    dt1, dt2 = sf1.dataType, sf2.dataType
     if dt1.typeName() == dt2.typeName():
         # Account for array types by inspecting elementType.
         if dt1.typeName() == 'array':
-            return dt1.elementType == dt2.elementType
+            return are_datatypes_equal_ignore_nullable(dt1.elementType, dt2.elementType)
         elif dt1.typeName() == 'struct':
-            return are_structfields_equal(sf1, sf2, True)
+            return are_schemas_equal_ignore_nullable(dt1, dt2)
         else:
             return True
     else:
--- tests/test_structfield_comparer.py
+++ tests/test_structfield_comparer.py
@@ -1,56 +1,44 @@
-import pytest
-
-from chispa.structfield_comparer import are_structfields_equal
 from pyspark.sql.types import *
 
+from chispa.schema_comparer import are_structfields_equal_ignore_nullable
 
-def describe_are_structfields_equal():
+def describe_are_structfields_equal_ignore_nullable():
     def it_returns_true_when_structfields_are_the_same():
         sf1 = StructField("hi", IntegerType(), True)
         sf2 = StructField("hi", IntegerType(), True)
-        assert are_structfields_equal(sf1, sf2) == True
+        assert are_structfields_equal_ignore_nullable(sf1, sf2) == True
 
     def it_returns_false_when_column_names_are_different():
         sf1 = StructField("hello", IntegerType(), True)
         sf2 = StructField("hi", IntegerType(), True)
-        assert are_structfields_equal(sf1, sf2) == False
-
-    def it_returns_false_when_nullable_property_is_different():
-        sf1 = StructField("hi", IntegerType(), False)
-        sf2 = StructField("hi", IntegerType(), True)
-        assert are_structfields_equal(sf1, sf2) == False
+        assert are_structfields_equal_ignore_nullable(sf1, sf2) == False
 
     def it_can_perform_nullability_insensitive_comparisons():
         sf1 = StructField("hi", IntegerType(), False)
         sf2 = StructField("hi", IntegerType(), True)
-        assert are_structfields_equal(sf1, sf2, ignore_nullability=True) == True
+        assert are_structfields_equal_ignore_nullable(sf1, sf2) == True
 
     def it_returns_true_when_nested_types_are_the_same():
         sf1 = StructField("hi", StructType([StructField("world", IntegerType(), False)]), False)
         sf2 = StructField("hi", StructType([StructField("world", IntegerType(), False)]), False)
-        assert are_structfields_equal(sf1, sf2) == True
+        assert are_structfields_equal_ignore_nullable(sf1, sf2) == True
 
     def it_returns_false_when_nested_names_are_different():
         sf1 = StructField("hi", StructType([StructField("world", IntegerType(), False)]), False)
         sf2 = StructField("hi", StructType([StructField("developer", IntegerType(), False)]), False)
-        assert are_structfields_equal(sf1, sf2) == False
+        assert are_structfields_equal_ignore_nullable(sf1, sf2) == False
 
     def it_returns_false_when_nested_types_are_different():
         sf1 = StructField("hi", StructType([StructField("world", IntegerType(), False)]), False)
         sf2 = StructField("hi", StructType([StructField("world", DoubleType(), False)]), False)
-        assert are_structfields_equal(sf1, sf2) == False
-
-    def it_returns_false_when_nested_types_have_different_nullability():
-        sf1 = StructField("hi", StructType([StructField("world", IntegerType(), False)]), False)
-        sf2 = StructField("hi", StructType([StructField("world", IntegerType(), True)]), False)
-        assert are_structfields_equal(sf1, sf2) == False
+        assert are_structfields_equal_ignore_nullable(sf1, sf2) == False
 
     def it_returns_false_when_nested_types_are_different_with_ignore_nullable_true():
         sf1 = StructField("hi", StructType([StructField("world", IntegerType(), False)]), False)
         sf2 = StructField("hi", StructType([StructField("developer", IntegerType(), False)]), False)
-        assert are_structfields_equal(sf1, sf2, ignore_nullability=True) == False
+        assert are_structfields_equal_ignore_nullable(sf1, sf2) == False
 
     def it_returns_true_when_nested_types_have_different_nullability_with_ignore_null_true():
         sf1 = StructField("hi", StructType([StructField("world", IntegerType(), False)]), False)
         sf2 = StructField("hi", StructType([StructField("world", IntegerType(), True)]), False)
-        assert are_structfields_equal(sf1, sf2, ignore_nullability=True) == True
+        assert are_structfields_equal_ignore_nullable(sf1, sf2) == True
