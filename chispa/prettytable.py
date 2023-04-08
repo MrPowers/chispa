b1cff47e9ce4bf951efea4c46c30e11714e4114f
Ethan Hill <5614778+ethnhll@users.noreply.github.com>
2023-04-08 12:50:26 -0400

    pkg_resources removed to stop deprecation warnings in output using prettytable

--- chispa/prettytable.py
+++ chispa/prettytable.py
@@ -41,9 +41,6 @@ import sys
 import textwrap
 import unicodedata
 
-import pkg_resources
-
-# __version__ = pkg_resources.get_distribution(__name__).version
 __version__ = "0.7.2"
 py3k = sys.version_info[0] >= 3
 if py3k:
