0e72963c35a181479df689498870c230ed15ca27
Matthew Powers <matthewkevinpowers@gmail.com>
2024-02-16 12:29:48 -0500

    demonstrate how custom formatting can be injected via pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from dataclasses import dataclass
+
+@dataclass
+class MyFormats:
+    mismatched_rows = ["light_yellow"]
+    matched_rows = ["cyan", "bold"]
+    mismatched_cells = ["purple", "blink"]
+    matched_cells = ["blue"]
+
+@pytest.fixture()
+def my_formats():
+    return MyFormats()
